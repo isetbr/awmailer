866af6e8f0e09a7b3b2414d4a37acf38cd228d42
Lucas Mendes <devsdmf@gmail.com>
2014-09-10 19:55:45 -0300

    Implemented unit tests in auth libraries

---
+++ tests/Iset/Api/Auth/IpAddressTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Iset\Api\Auth;
+
+use Iset\Api\Auth\IpAddress;
+
+class IpAddressTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testInitialize()
+    {
+        $auth = new IpAddress(\App::configure());
+        $this->assertInstanceOf('Iset\Api\Auth\IpAddress',$auth);
+        return $auth;
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testInitializeFail()
+    {
+        $auth = new IpAddress();
+    }
+
+    public function testAllowIpAddress()
+    {
+        $ipaddress = new \Iset\Api\Resource\IpAddress(new \Iset\Model\IpAddressTable(\App::configure()));
+        $ipaddress->ipaddress = '127.0.1.1';
+        $this->assertTrue($ipaddress->save());
+        return $ipaddress;
+    }
+
+    /**
+     * @depends testInitialize
+     * @depends testAllowIpAddress
+     */
+    public function testAuth()
+    {
+        $auth = func_get_arg(0);
+        $ipaddress = func_get_arg(1);
+
+        $this->assertInstanceOf('Iset\Api\Resource\IpAddress',$auth->validate($ipaddress->ipaddress));
+        $ipaddress->delete();
+    }
+
+    /**
+     * @depends testInitialize
+     * @depends testAllowIpAddress
+     */
+    public function testAuthFail()
+    {
+        $auth = func_get_arg(0);
+        $ipaddress = func_get_arg(1);
+
+        $this->assertFalse($auth->validate($ipaddress->ipaddress));
+    }
+}
