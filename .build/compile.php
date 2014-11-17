a9608c441cf54fea1535c3d1cf0025334aaf3f02
devsdmf <devsdmf@gmail.com>
2014-11-17 17:13:15 -0200

    Fixed php binary path generation

--- .build/compile.php
+++ .build/compile.php
@@ -1,29 +1,33 @@
 #!/usr/bin/php
 <?php
 
+# Initializing paths
 $root_path = realpath(dirname(__FILE__) . '/../');
 $bin_path = $root_path . '/bin/';
 $daemon = $bin_path . "awd";
 $service = $bin_path . "awmailer";
+$php_binary = trim(exec('which php'));
 
+# Removing existing daemon
 if (file_exists($daemon)) {
     unlink($daemon);
 }
 
 $content = <<<EOF
-#!/usr/bin/php -q
+#!$php_binary -q
 <?php require_once("$root_path/app/Daemon.php");
 EOF;
 $handle = fopen($daemon,"w");
 fwrite($handle,$content);
 fclose($handle);
 
+# Removing existing service
 if (file_exists($service)) {
     unlink($service);
 }
 
 $content = <<<EOF
-#!/usr/bin/php -q
+#!$php_binary -q
 <?php require_once("$root_path/app/Service.php");
 EOF;
 $handle = fopen($service,"w");
